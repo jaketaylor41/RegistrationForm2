e10d6ea20d979082033d78483371b9c09d774ef9
Jacob Taylor <jacobtaylor@Jacobs-MacBook-Pro.local>
2018-09-11 14:13:10 -0700

    database

--- server.php
+++ server.php
@@ -10,27 +10,28 @@ $database = ltrim($dbparts['ecdupp1z6rgjtuqa'],'/');
 
 
 // Create connection
-$connection = mysqli_connect($hostname, $username, $password, $database);
+$link = mysqli_connect($hostname, $username, $password, $database);
 
 // Check connection
-if ($connection->connect_error) {
-    die("Connection failed: " . $connection->connect_error);
+if ($link->connect_error) {
+    die("Connection failed: " . $link->connect_error);
 }
 echo "Connection was successfully established!";
+
 
 
 $errors = array();
 // REGISTER USER
 if (isset($_POST['reg_user'])) {
     // receive all input values from the form
-    $firstName = mysqli_real_escape_string($connection, $_POST['fNameInput']);
-    $lastName = mysqli_real_escape_string($connection, $_POST['lNameInput']);
-    $personEmail = mysqli_real_escape_string($connection, $_POST['emailInput']);
-    $personConfirmEmail = mysqli_real_escape_string($connection, $_POST['confirmEmail']);
-    $personBirthday = mysqli_real_escape_string($connection, $_POST['dobInput']);
-    $personUsername = mysqli_real_escape_string($connection, $_POST['usernameInput']);
-    $personPassword = mysqli_real_escape_string($connection, $_POST['passwordInput']);
-    $personConfirmPassword = mysqli_real_escape_string($connection, $_POST['confirmPassword']);
+    $firstName = mysqli_real_escape_string($link, $_POST['fNameInput']);
+    $lastName = mysqli_real_escape_string($link, $_POST['lNameInput']);
+    $personEmail = mysqli_real_escape_string($link, $_POST['emailInput']);
+    $personConfirmEmail = mysqli_real_escape_string($link, $_POST['confirmEmail']);
+    $personBirthday = mysqli_real_escape_string($link, $_POST['dobInput']);
+    $personUsername = mysqli_real_escape_string($link, $_POST['usernameInput']);
+    $personPassword = mysqli_real_escape_string($link, $_POST['passwordInput']);
+    $personConfirmPassword = mysqli_real_escape_string($link, $_POST['confirmPassword']);
     // form validation: ensure that the form is correctly filled ...
     // by adding (array_push()) corresponding error unto $errors array
     if (empty($firstName)) { array_push($errors, "First name is required"); }
@@ -50,7 +51,7 @@ if (isset($_POST['reg_user'])) {
     // first check the database to make sure
     // a user does not already exist with the same username and/or email
     $user_check_query = "SELECT * FROM db WHERE username='$personUsername' OR email='$personEmail' LIMIT 1";
-    $result = mysqli_query($connection, $user_check_query);
+    $result = mysqli_query($link, $user_check_query);
     $user = mysqli_fetch_assoc($result);
     if ($user) { // if user exists
         if ($user['username'] === $personUsername) {
@@ -64,7 +65,7 @@ if (isset($_POST['reg_user'])) {
     if (count($errors) == 0) {
         $password = md5($personPassword);//encrypt the password before saving in the database
         $query = "INSERT INTO `db` (`id`, `fName`, `lName`, `email`, `confirmEmail`, `birthday`, `username`, `password`, `confirmPassword`) VALUES (NULL, '$firstName', '$lastName', '$personEmail', '$personConfirmEmail', '$personBirthday', '$personUsername', '$personPassword', '$personConfirmPassword')";
-        mysqli_query($connection, $query);
+        mysqli_query($link, $query);
         $_SESSION['username'] = $personUsername;
         $_SESSION['success'] = "You are now logged in";
         header('location: home.php');
