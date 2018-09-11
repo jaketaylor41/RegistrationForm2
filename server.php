9717708705ae596fe12a7699610fbe3a3028c6f5
Jacob Taylor <jacobtaylor@Jacobs-MacBook-Pro.local>
2018-09-11 08:57:57 -0700

    database

--- server.php
+++ server.php
@@ -12,11 +12,11 @@ $database = ltrim($dbparts['ecdupp1z6rgjtuqa'],'/');
 
 
 // Create connection
-$conn = new mysqli($hostname, $username, $password, $database);
+$connection = new mysqli($hostname, $username, $password, $database);
 
 // Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+if ($connection->connect_error) {
+    die("Connection failed: " . $connection->connect_error);
 }
 echo "Connection was successfully established!";
 
