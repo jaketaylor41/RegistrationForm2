c02347fb73024d2ee226ca15675364a8d62c6f9d
Jacob Taylor <jacobtaylor@Jacobs-MacBook-Pro.local>
2018-09-11 08:56:02 -0700

    database

--- server.php
+++ server.php
@@ -51,7 +51,7 @@ if (isset($_POST['reg_user'])) {
     }
     // first check the database to make sure
     // a user does not already exist with the same username and/or email
-    $user_check_query = "SELECT * FROM heroku_table WHERE username='$personUsername' OR email='$personEmail' LIMIT 1";
+    $user_check_query = "SELECT * FROM users WHERE username='$personUsername' OR email='$personEmail' LIMIT 1";
     $result = mysqli_query($connection, $user_check_query);
     $user = mysqli_fetch_assoc($result);
     if ($user) { // if user exists
@@ -65,7 +65,7 @@ if (isset($_POST['reg_user'])) {
     // Finally, register user if there are no errors in the form
     if (count($errors) == 0) {
         $password = md5($personPassword);//encrypt the password before saving in the database
-        $query = "INSERT INTO `heroku_table` (`id`, `fName`, `lName`, `email`, `confirmEmail`, `birthday`, `username`, `password`, `confirmPassword`) VALUES (NULL, '$firstName', '$lastName', '$personEmail', '$personConfirmEmail', '$personBirthday', '$personUsername', '$personPassword', '$personConfirmPassword')";
+        $query = "INSERT INTO `users` (`id`, `fName`, `lName`, `email`, `confirmEmail`, `birthday`, `username`, `password`, `confirmPassword`) VALUES (NULL, '$firstName', '$lastName', '$personEmail', '$personConfirmEmail', '$personBirthday', '$personUsername', '$personPassword', '$personConfirmPassword')";
         mysqli_query($connection, $query);
         $_SESSION['username'] = $personUsername;
         $_SESSION['success'] = "You are now logged in";
