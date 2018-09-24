1dd68675bd1e7cd7dfb302295021e5f41a75b8d9
Jacob Taylor <jacobtaylor@Jacobs-MacBook-Pro-2.local>
2018-09-23 22:15:44 -0700

    Added headers

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
+
+<!--Project Name: Registration Form V1
+//Module Name: php7_module
+//Programmers: Jacob Taylor, Hermes Mimini, Isaiah Camacho, Isaiah DeBenedetto
+//Date: 9/23/18
+//Synopsis of php7_module: PHP 7 uses new Zend Engine 3.0 to improve application performance almost twice and 50% better memory consumption than PHP 5.6. It allows to serve more concurrent users without requiring any additional hardware.
+//References: https://www.tutorialspoint.com/php7/index.htm, https://www.w3schools.com/php/,
+// Welling, L., & Thomson, L. (2008). PHP and MySQL web development (4th ed.). Boston, MA: Addison-Wesley Professional. ISBN-13: 9780672329166-->
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
