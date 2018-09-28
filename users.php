27a5412977421f4f768c88d56bfc5928cc893500
Jacob Taylor <jacobtaylor@Jacobs-MacBook-Pro-2.local>
2018-09-27 17:04:49 -0700

    Added headers

--- users.php
+++ users.php
@@ -13,10 +13,10 @@
 
 <?php
 
-//$hostname = "bbj31ma8tye2kagi.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306";
-//$username = "c6yha5d7xaec2saa";
-//$password = "mmz42r0bv1ukt52b";
-//$database = "ecdupp1z6rgjtuqa";
+$hostname = "bbj31ma8tye2kagi.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306";
+$username = "c6yha5d7xaec2saa";
+$password = "mmz42r0bv1ukt52b";
+$database = "ecdupp1z6rgjtuqa";
 
 //declare database variables
 //isaiah's personal database
@@ -25,11 +25,11 @@ $username = "root";
 $password = "root";
 $database = "form-demo"; */
 
-//jake's local database
-$hostname = "localhost";
-$username = "root";
-$password = "root";
-$database = "heroku";
+////jake's local database
+//$hostname = "localhost";
+//$username = "root";
+//$password = "root";
+//$database = "heroku";
 
 //establish db connect
 $conn = new mysqli($hostname, $username, $password, $database);
@@ -54,7 +54,7 @@ if ($result = $conn->query($sql_statement)) {
         header("Location: home.php");
     }
     else {
-        echo '<div class="login-error">incorrect login</div>';
+        echo '<alert class="login-error">incorrect login</alert>';
         include("index.php");
         //header("Location: index.php");
     }
