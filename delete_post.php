28260d399b4fd20dd9bc9cb9ced04cb9cd6ee5f2
Jacob Taylor <jacobdeantaylor@gmail.com>
2018-10-21 23:14:14 -0700

    test

--- delete_post.php
+++ delete_post.php
@@ -1,10 +1,15 @@
 <?php
 
 //isaiah's local database
-$hostname = "localhost";
-$username = "root";
-$password = "root";
-$database = "form-demo";
+//$hostname = "localhost";
+//$username = "root";
+//$password = "root";
+//$database = "form-demo";
+
+$hostname = "bbj31ma8tye2kagi.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306";
+$username = "c6yha5d7xaec2saa";
+$password = "mmz42r0bv1ukt52b";
+$database = "ecdupp1z6rgjtuqa";
 
 $conn = mysqli_connect($hostname, $username, $password, $database);
 
